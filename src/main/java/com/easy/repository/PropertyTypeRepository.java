a2e002ca677cd2d17471584c3335780ea1653c19
Praneeth <praneeth.reddy>
2025-10-08 00:04:18 +0530

    changes

--- src/main/java/com/easy/repository/PropertyTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.easy.repository;
-
-import com.easy.entity.PropertyTypeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PropertyTypeRepository extends JpaRepository<PropertyTypeEntity,Long> {
-}
