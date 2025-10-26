1c3f83b147faa10f5aff82ad382702b850b6e693
Praneeth <praneeth.reddy>
2025-10-26 14:05:04 +0530

    ping Scheduler

--- src/main/java/com/easy/repository/ImageDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.easy.repository;
-
-import com.easy.entity.ImageDetailsEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ImageDetailsRepository  extends JpaRepository<ImageDetailsEntity,Long> {
-}
